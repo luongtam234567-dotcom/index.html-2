22c121dea0c74f59a3ac5a536dc8354dd2f4df3c
TamVanLuong <tamvanluong@TamVanLuongs-MacBook-Pro.local>
2026-08-13 21:59:16 -0500

    updated them all

---
+++ app.js
@@ -0,0 +1,11 @@
+async function firstSixIncomplete(userId) {
+    const promise = fetch("https://jsonplaceholder.typicode.com/todos");
+
+    const result = await promise.json()
+
+    const incompletetasks = result.filter(elem => elem.completed).slice(0, 6);
+
+    console.log(incompletetasks);
+}
+
+firstSixIncomplete(6);
